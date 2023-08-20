b8da9cf4251046506e94df59c19d72334e0e3bcb
kyawzinhtet <KyawZinHtet200116@outlook.com>
2023-08-20 09:02:38 +0630

    changes for static review

--- MBlogCore.Web/Controllers/ReviewController.cs
+++ MBlogCore.Web/Controllers/ReviewController.cs
@@ -1,22 +1,40 @@
 ﻿using System;
+using MBlogCore.Persistance.Context;
+using MBlogCore.Persistance.Tables;
+using MBlogCore.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace MBlogCore.Web.Controllers
 {
-	public class ReviewController
+	public class ReviewController:ControllerBase
 	{
-		public ReviewController()
+		private MBlogContext context;
+		public ReviewController(MBlogContext context)
 		{
+			this.context = context;
 		}
 		[HttpGet]
 		public IActionResult Index()
 		{
-			return null;
+			return Ok("Hello World");
 		}
 		[HttpPost]
-		public IActionResult Insert()
+		public IActionResult Insert(ReviewViewModel model)
 		{
-			return null;
+			if (ModelState.IsValid)
+			{
+				ReviewTable table = new ReviewTable
+				{
+					Message = model.Message,
+					BlogId = Guid.Parse(model.BlogId),
+					UserId=Guid.NewGuid()
+				};
+				context.reviews.Add(table);
+				context.SaveChanges();
+				return Ok("Saved Successfully");
+			}
+			return BadRequest("something wrong");
 		}
 	}
 }
