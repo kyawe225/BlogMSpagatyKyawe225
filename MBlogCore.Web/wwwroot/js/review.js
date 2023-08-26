94e7dadc515dbdb3d921cadfa7b55ede847a1afa
kyawzinhtet <KyawZinHtet200116@outlook.com>
2023-08-26 16:23:22 +0630

    review added

---
+++ MBlogCore.Web/wwwroot/js/review.js
@@ -0,0 +1,20 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    getReview();
+});
+
+let reviewButton = document.getElementById("confirm-but");
+
+reviewButton.addEventListener("click", function (ev) {
+    let message = document.getElementById("review-mes");
+    if (message) {
+        
+    }
+);
+
+
+
+function getReview(string message) {
+    fetch("review/").then(resposne => response.json()).then((data) =>
+        console.log(data);
+    );
+}
