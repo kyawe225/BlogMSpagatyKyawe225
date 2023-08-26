cf2c51b55ceb04e385e30498d63fcfd9e7a4f3fb
kyawzinhtet <KyawZinHtet200116@outlook.com>
2023-08-26 13:10:24 +0630

    user profile edit added

--- MBlogCore.Web/Controllers/UserController.cs
+++ MBlogCore.Web/Controllers/UserController.cs
@@ -1,27 +1,78 @@
 ﻿
+using MBlogCore.Persistance.Context;
+using MBlogCore.Persistance.Tables;
+using MBlogCore.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace MBlogCore.Web.Controllers
 {
-	public class UserController:Controller
-	{
-		public UserController()
-		{
-		}
-		public IActionResult Profile()
-		{
-			HttpContext.User.Identity.;
-			return null;
-		}
-		public IActionResult ProfileEdit()
-		{
-			return null;
-		}
-		public IActionResult SavedBlogs()
-		{
-			return null;
-		}
-	}
+    public class UserController : Controller
+    {
+        private readonly MBlogContext context;
+        public UserController(MBlogContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        [Authorize]
+        public IActionResult Profile()
+        {
+            Claim? s = User.FindFirst(ClaimTypes.NameIdentifier);
+            Guid userId = Guid.Parse(s.Value);
+            UserDetailViewModel user = context.users.Where(p => p.Id == userId).Select(p => new UserDetailViewModel
+            {
+                Name = p.Name,
+                Address = p.Address,
+                PhoneNumber = p.PhoneNumber,
+                DOB = p.DOB == null ? null : p.DOB,
+                Email = p.Email,
+                IsActive = p.IsActive,
+                IsFullyActivated = p.IsFullyActivated
+            }).First();
+            return View("Profile", user);
+        }
+        [HttpGet]
+        [Authorize]
+        public IActionResult ProfileEdit()
+        {
+            Claim? s = User.FindFirst(ClaimTypes.NameIdentifier);
+            Guid userId = Guid.Parse(s.Value);
+            UserUpdateViewModel user = context.users.AsNoTracking().Where(p => p.Id == userId).Select(p => new UserUpdateViewModel
+            {
+                Name = p.Name,
+                Address = p.Address,
+                PhoneNumber = p.PhoneNumber == null ? "" : p.PhoneNumber,
+                Email = p.Email,
+            }).First();
+            return View("Edit", user);
+        }
+        [HttpPost]
+        [Authorize]
+        [ActionName("ProfileEdit")]
+        public IActionResult ProfileEditAction(UserUpdateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Claim? s = User.FindFirst(ClaimTypes.NameIdentifier);
+                Guid userId = Guid.Parse(s.Value);
+                User user = context.users.Where(p => p.Id == userId).First();
+                user.Name = model.Name;
+                user.Email = model.Email;
+                user.Address = model.Address;
+                user.DOB = model.DOB;
+                user.PhoneNumber = model.PhoneNumber;
+                user.IsFullyActivated = true;
+                context.users.Update(user);
+                context.SaveChanges();
+                TempData["SuccessMessage"] = "Successfully updated";
+                return RedirectToAction("Profile", "User");
+            }
+            TempData["ErrorMessage"] = "Not Successfully";
+            return View("Edit", model);
+        }
+    }
 }
 
---
+++ MBlogCore.Web/Models/UserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace MBlogCore.Web.Models
+{
+	public class UserDetailViewModel
+	{
+		public string Name { set; get; }
+		public bool IsActive { set; get; }
+		public string Email { set; get; }
+		public string? PhoneNumber { set; get; }
+		public DateTime? DOB { set; get; }
+		public string? Address { set; get; }
+		public bool IsFullyActivated { set; get; }
+		public UserDetailViewModel()
+		{
+		}
+	}
+	public class UserUpdateViewModel
+	{
+        public string Name { set; get; }
+        public string Email { set; get; }
+        public string PhoneNumber { set; get; }
+        public DateTime DOB { set; get; }
+        public string? Address { set; get; }
+    }
+}
+
